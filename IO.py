5c577829e76a383a0dfa48503a400febc035c269
qiwulun <qiwulun08@gmail.com>
2017-03-06 16:10:24 +0800

    update

---
+++ IO.py
@@ -0,0 +1,35 @@
+import glob
+import os
+inputPath = './posts/*.org'
+outputPath = r'~/git/hexo_demo/source/_posts/'
+
+a = glob.glob(inputPath)[:10]
+
+for iname in a:
+    filename = ""
+    filecontent=""
+    with open(iname,"r") as ifile :
+        for line in ifile.readlines():
+            # print(line)
+            if "#+BEGIN_COMMENT" in line or "#+END_COMMENT" in line or ".. slug:" in line or '# -*- mode: Org; org-download-image-dir: "../images"; -*-' in line:
+                pass
+            else:
+                line=line.replace(".. title:","#+TITLE:")
+                line=line.replace(".. date:","#+DATE:")
+                line=line.replace("UTC+08:00", "" )
+                line=line.replace(".. tags:","#+TAGS:")
+                line=line.replace(".. category:","#+CATEGORY:")
+                line=line.replace(".. link:","#+LINK:")
+                line=line.replace(".. description:","#+DESCRIPTION:")
+                line=line.replace(".. type: text","#+LAYOUT : post")
+                line=line.replace(".. type: micro","#+LAYOUT : post") 
+                line=line.replace("../images","images") 
+                filecontent=filecontent+line
+            if "../images" in line:
+                print(line)
+            if "#+TITLE" in line:
+                filename = line.replace("#+TITLE:","").strip()
+    # os.renames(iname, "./posts/"+filename+".org")
+
+    with open(iname,"w") as f:
+        f.writelines(filecontent)
